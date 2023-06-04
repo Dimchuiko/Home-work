88b93bc97f9518561e0f8c7d85b23b67bf04b8b4
Dim <dimchuiko@gmail.com>
2023-06-04 11:26:52 +0200

    create folder Homework

---
+++ js/js.js
@@ -0,0 +1,8 @@
+"use stricts";
+
+// let header = document.querySelector("h1");
+// let secondHeader = document.createElement("h2");
+
+// secondHeader.innerHTML = "Ich libe dich";
+// header.after(secondHeader);
+// console.log(secondHeader.innerHTML);
